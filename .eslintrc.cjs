92d6513e3d3685f4a229ed12738fbc0e2fe2e262
Luka Prebil Grintal <pgluka7@gmail.com>
2024-01-04 15:02:57 +0100

    Add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+/**
+ * @type {import("eslint").Linter.Config}
+ */
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    requireConfigFile: false,
+    babelOptions: {
+      plugins: ["@babel/plugin-syntax-import-assertions"],
+    },
+  },
+  rules: {},
+};
