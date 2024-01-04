739aed16870c30dcafc9026fbc86cd3d3a77b55e
Luka Prebil Grintal <pgluka7@gmail.com>
2024-01-04 15:04:31 +0100

    handle public holidays and measure performance

--- src/getPrice.js
+++ src/getPrice.js
@@ -1,19 +1,57 @@
+import { performance } from "node:perf_hooks";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc.js";
 import tz from "dayjs/plugin/timezone.js";
 import config from "../config.json" assert { type: "json" };
+import { log } from "./app.js";
 
 dayjs.extend(utc);
 dayjs.extend(tz);
 
-function getBlock() {
-  const nowInSlovenia = dayjs().tz("Europe/Ljubljana");
+
+/**
+ * Check if today is a public holiday in Slovenia.
+ * @param {dayjs.Dayjs} nowInSlovenia
+ * @returns
+ */
+async function isTodayPublicHoliday(nowInSlovenia) {
+  const utcOffset = Math.floor(nowInSlovenia.utcOffset() / 60);
+  log.info(`UTC offset: ${utcOffset}`);
+  const url = `https://date.nager.at/api/v3/IsTodayPublicHoliday/SI?offset=${utcOffset}`;
+  const response = await fetch(url);
+  log.info(`IsTodayPublicHoliday Response status: ${response.status}`);
+  switch (response.status) {
+    case 200:
+      return true;
+    case 204:
+      return false;
+    default:
+      log.error(`Unexpected response status: ${response.status} when determining if today is a public holiday.`);
+      throw new Error(
+        `Unexpected response status: ${response.status} when determining if today is a public holiday.`
+      );
+  }
+}
+
+/**
+ * @param {dayjs.Dayjs} nowInSlovenia
+ * @param {boolean} isWorkingDay
+ */
+async function getBlock(nowInSlovenia, isWorkingDay) {
   const currentMonth = nowInSlovenia.month() + 1;
   const season = config.season.hi_season.includes(currentMonth)
     ? "hi_season"
     : "low_season";
 
-  const block = config.blocks[season].work_day[nowInSlovenia.hour()];
+  performance.mark(`isTodayPublicHolidayStart`);
+  const isPublicHoliday = await isTodayPublicHoliday(nowInSlovenia);
+  performance.mark(`isTodayPublicHolidayEnd`);
+  performance.measure(`isTodayPublicHoliday`, `isTodayPublicHolidayStart`, `isTodayPublicHolidayEnd`);
+  log.debug(`isPublicHoliday measure: ${performance.getEntriesByName("isTodayPublicHoliday")[0].duration}`);
+  const blockKey = isPublicHoliday || !isWorkingDay ? "weekend" : "work_day";
+  log.info(`Season: ${season}, blockKey: ${blockKey}`);
+
+  const block = config.blocks[season][blockKey][nowInSlovenia.hour()];
   return block;
 }
 
@@ -21,7 +59,16 @@ function getBlock() {
  * @param {string} provider
  */
 export async function getPrice(provider) {
-  const block = getBlock();
+  log.info(`Getting price for ${provider}`)
+  const nowInSlovenia = dayjs().tz("Europe/Ljubljana");
+  const dayOfWeek = nowInSlovenia.day();
+  const isWorkingDay = dayOfWeek >= 1 && dayOfWeek <= 5;
+  log.info(`Now in Slovenia: ${nowInSlovenia.format()}, isWorkingDay: ${isWorkingDay}`)
 
-  return `B${block}`;
+  performance.mark(`getBlockStart-${provider}`);
+  const block = await getBlock(nowInSlovenia, isWorkingDay);
+  performance.mark(`getBlockEnd-${provider}`);
+  performance.measure(`getBlock-${provider}`, `getBlockStart-${provider}`, `getBlockEnd-${provider}`);
+  log.debug(`getBlock measure: ${performance.getEntriesByName(`getBlock-${provider}`)[0].duration}`);
+  return `${provider} -> B${block}`;
 }
--- src/index.js
+++ src/index.js
@@ -4,18 +4,23 @@ import { fastify, log } from "./app.js";
 
 
 fastify.get("/:provider", async (request) => {
+  performance.mark(`start-/${request.params.provider}`);
 
-fastify.get("/:provider", async (request, reply) => {
   const { provider } = request.params;
-  return await getPrice(provider);
+  const result = await getPrice(provider);
+
+  performance.mark(`end-/${request.params.provider}`);
+  performance.measure(`/${request.params.provider}`, `start-/${request.params.provider}`, `end-/${request.params.provider}`);
+  log.debug(`/${request.params.provider} measure: ${performance.getEntriesByName(`/${request.params.provider}`)[0].duration}`);
+  return result;
 });
 
 const start = async () => {
   try {
-      await fastify.listen({ port: 3000 })
+    await fastify.listen({ port: 3000 });
   } catch (err) {
     log.error(err);
-      process.exit(1)
+    process.exit(1);
   }
 };
 
